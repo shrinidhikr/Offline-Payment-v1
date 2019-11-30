890716ba9a17bece5ddb785eaee1dad47790266c
Sneha V <32172246+snehavishwanatha@users.noreply.github.com>
2019-11-30 12:04:34 +0530

    Update database_trigger.js

--- Firebase_deployment/database_trigger.js
+++ Firebase_deployment/database_trigger.js
@@ -1,10 +1,42 @@
 const functions = require('firebase-functions');
 
-exports.databaseTrigger = functions.database.ref('/Transactions/')
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.databaseTrigger = functions.database.ref('/Transactions/{keyId}/')
     .onWrite((change, context) => {
     
       // Grab the current value of what was written to the Realtime Database.
-      const original = change.after.val();
-      console.log('Uppercasing', context.params.pushId, original);
+      const cipher = change.after.val();
+      console.log('Uppercasing', context.params.keyId, cipher);
+      
+      var  ref = firebase.database().ref("Registrations/"+context.params.keyId+"/D");
+       ref.on("value",function(snapshot){console.log(snapshot.val());},function(error){console.log("Error",error)});
+      
+     
     });
     
+    
+   /* admin.database().ref('/users/'+event.params.id+'/NotificationToken/IOS').once('v‌​alue').then((snapshot) => { 
+var token=snapshot.val().IOS;
+
+ });*/
+/*exports.helloWorld = functions.https.onRequest((req, res) => {
+
+    var cipher = 11;
+    var d = 2;
+    var n = 2;
+    var result = Math.pow(cipher, d);
+    result = result % n;
+    console.log(result.toString());
+    
+    res.send("Plain text after decryption is ".concat(result.toString()));
+    
+});*/
+
+
