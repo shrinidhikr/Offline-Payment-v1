b5bfcc52787d6737a30cebc6b6b4bb30587049b7
Sneha V <32172246+snehavishwanatha@users.noreply.github.com>
2019-11-30 19:55:14 +0530

    Update index.js

--- final_decryption/index.js
+++ final_decryption/index.js
@@ -7,7 +7,7 @@ admin.initializeApp();
 const db = admin.database();
 
 
-exports.transactD = functions.database.ref('/Transactions/{keyId}/').onWrite((change,context) => {
+exports.transactD = functions.database.ref('/Transactions/{keyId}/').onUpdate((change,context) => {
 
       const cipher = change.after.val();
       console.log('Cipher',context.params.keyId, cipher);
@@ -21,8 +21,7 @@ exports.transactD = functions.database.ref('/Transactions/{keyId}/').onWrite((ch
        },function(error){
        console.log(error);
        });
-        
-       
+         
        var ref = db.ref("/Registration/"+context.params.keyId.toString()+"/n");
        ref.on("value",function(snapshot){
        var n = snapshot.val();
@@ -34,11 +33,49 @@ exports.transactD = functions.database.ref('/Transactions/{keyId}/').onWrite((ch
        const decrypted_message = RSA.decrypt(cipher, d, n);
        const decoded_message = RSA.decode(decrypted_message);
        
+       var message = decoded_message.split(" ");
        
+       var sender_acc = context.params.keyId.toString();
+       
+       var ref = db.ref("/Phone_Acc/"+message[0]);
+       ref.on("value",function(snapshot){
+       var receiver_acc = snapshot.val().toString();
+       console.log("Receiver accno", receiver_acc);
+       },function(error){
+       console.log(error);
+       });
+       
+       var transaction_amount = parseInt(message[1]);       
+       
+       var ref = db.ref("/Balance/"+sender_acc);
+       ref.on("value",function(snapshot){
+       var sender_balance = snapshot.val().toString();
+       console.log("Sender bal", sender_balance);
+       },function(error){
+       console.log(error);
+       });
+       
+       var ref = db.ref("/Balance/"+receiver_acc);
+       ref.on("value",function(snapshot){
+       var receiver_balance = snapshot.val().toString();
+       console.log("Receiver bal", receiver_balance);
+       },function(error){
+       console.log(error);
+       });
+      
+       var ref = db.ref("/Balance");
+       ref.update({
+          sender_acc: parseInt(sender_balance) - parseInt(transaction_amount);
+       });
+      
+       var ref = db.ref("/Balance");
+       ref.update({
+          receiver_acc: parseInt(receiver_balance) + parseInt(transaction_amount);
+       });
        
        
     });
-    
+     
 
 
  
